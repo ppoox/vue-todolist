d45dea1b2bab5ddeb5e24804f2728eeed5aeaaa3
DiSFiral <ppoox@naver.com>
2019-12-12 20:05:43 +0900

    vue-todolist

---
+++ src/App.vue
@@ -0,0 +1,79 @@
+<template>
+  <div id="app">
+    <TodoHeader></TodoHeader>
+    <TodoInput @addEvent="addTodo"></TodoInput>
+    <TodoList :todo-array="todoItems" @removeEvent="removeTodo" @toggleEvent="toggleTodo"></TodoList>
+    <TodoFooter @clearEvent="clearTodo"></TodoFooter>
+  </div>
+</template>
+
+<script>
+import TodoHeader from "./components/TodoHeader.vue";
+import TodoInput from "./components/TodoInput.vue";
+import TodoList from "./components/TodoList.vue";
+import TodoFooter from "./components/TodoFooter.vue";
+
+export default {
+  components: {
+    TodoHeader,
+    TodoInput,
+    TodoList,
+    TodoFooter
+  },
+  data() {
+    return {
+      todoItems: []
+    };
+  },
+  methods: {
+    addTodo(todoItem) {
+      let obj = { completed: false, item: todoItem };
+      localStorage.setItem(todoItem, JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+
+    removeTodo(todoItem, index) {
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1);
+    },
+    toggleTodo(todoItem, index) {
+      this.todoItems[index].completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearTodo() {
+      this.todoItems = [];
+      localStorage.clear();
+    }
+  },
+  /* life cycle method */
+  created() {
+    if (localStorage.length > 0) {
+      for (let i = 0; i < localStorage.length; i++) {
+        if (localStorage.key(i) !== "loglevel:webpack-dev-server") {
+          this.todoItems.push(
+            JSON.parse(localStorage.getItem(localStorage.key(i)))
+          );
+        }
+      }
+    }
+  }
+};
+</script>
+
+<style>
+body {
+  text-align: center;
+  background-color: #f6f6f6;
+}
+input {
+  border-style: groove;
+  width: 200px;
+}
+button {
+  border-style: groove;
+}
+.shadow {
+  box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
---
+++ src/components/TodoInput.vue
@@ -0,0 +1,58 @@
+<template>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" v-on:keyup.enter="addTodo">
+        <span class="addContainer" v-on:click="addTodo">
+            <i class="fas fa-plus addBtn"></i>
+        </span>
+        <!-- <button @click="addTodo">add</button> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            newTodoItem: ""
+        }
+    },
+    methods:{
+      addTodo() {
+        if (this.newTodoItem !== "") {
+          this.$emit("addEvent", this.newTodoItem);
+          this.clearInput();
+        }
+      },
+      clearInput() {
+      //addTodo() 함수에 있었지만 clearInput()함수로 이동
+      this.newTodoItem = "";
+    }
+    }
+}
+</script>
+
+<style scoped>
+input:focus {
+  outline: none;
+}
+.inputBox {
+  background: white;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 5px;
+}
+.inputBox input {
+  border-style: none;
+  font-size: 0.9rem;
+}
+.addContainer {
+  float: right;
+  background: linear-gradient(to right, #6478FB, #8763FB);
+  display: block;
+  width: 3rem;
+  border-radius: 0 5px 5px 0;
+}
+.addBtn {
+  color: white;
+  vertical-align: middle;
+}
+</style>
